d8e22c8bc025226d6c718d3df0671eda520d0f48
Flora72 <zahratheo569@gmail.com>
2025-11-24 12:57:14 +0300

    feat: add views for rendering core templates and handling incident logic

--- vioApp/views.py
+++ vioApp/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+
 
 # Create your views here.
+def index (request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def emergency(request):
+    return render(request,'emergency.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def password_reset(request):
+    return render(request,'password_reset.html')
+
+def report_incident(request):
+    return render(request, 'report_incident.html')
+
+def resources(request):
+    return render(request,'resources.html')
+
+def incident_history(request):
+    return render(request,'incident_history.html')
+
+def dashboard(request):
+    return render(request,'dashboard.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
